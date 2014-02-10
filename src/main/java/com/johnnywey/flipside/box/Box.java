b7ff9642f8765ec29bbc8b5ffeefd60e2861afa7
Johnny Wey <johnnywey@gmail.com>
2014-02-10 10:25:07 -0700

    Fix javadoc

--- src/main/java/com/johnnywey/flipside/box/Box.java
+++ src/main/java/com/johnnywey/flipside/box/Box.java
@@ -2,7 +2,7 @@ package com.johnnywey.flipside.box;
 
 /**
  * Wrapped interface for a Box. Meant to be similar to a Scala Option
- * (see {@link http://en.wikipedia.org/wiki/Option_type}).
+ * (see <a href="http://en.wikipedia.org/wiki/Option_type">http://en.wikipedia.org/wiki/Option_type</a>}).
  * <p/>
  * A Box is either full or empty. If full, you can call .get() and retrieve the value. If empty,
  * calling .get() will throw an exception.
